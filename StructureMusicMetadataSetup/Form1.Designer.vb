d3caa034db6358b4c13968e14126ebb8c058422e
Lukas Kurz <lukas@kurzweb.de>
2020-08-07 20:46:40 +0200

    Projektdateien hinzufügen.

---
+++ StructureMusicMetadataSetup/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(31, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(319, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "C:\Users\lukas\Music\test\"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(31, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(319, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Set Metadata"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(381, 111)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ StructureMusicMetadataSetup/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+Imports Microsoft.WindowsAPICodePack.Shell
+Imports Microsoft.WindowsAPICodePack.Shell.PropertySystem
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        With My.Computer.FileSystem
+
+            For Each InterpretDir In .GetDirectories(TextBox1.Text)
+                For Each AlbumDir In .GetDirectories(InterpretDir)
+                    For Each FilePath In .GetFiles(AlbumDir).Where(Function(x) Path.GetExtension(x) = ".mp3")
+                        Dim file = ShellObject.FromParsingName(FilePath)
+                        Dim properties = file.Properties.GetPropertyWriter()
+                        properties.WriteProperty(SystemProperties.System.Music.AlbumTitle, Path.GetFileName(AlbumDir))
+                        properties.WriteProperty(SystemProperties.System.Music.Artist, Path.GetFileName(InterpretDir))
+                        properties.WriteProperty(SystemProperties.System.Title, Path.GetFileNameWithoutExtension(FilePath))
+                        properties.Close()
+                    Next
+                Next
+            Next
+
+        End With
+
+    End Sub
+End Class
